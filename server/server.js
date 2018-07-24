3287e78ed4fdee7577027d7d58cfa8423aba7e3a
tmat06 <timmy.a.matthews@gmail.com>
2018-07-24 17:29:09 -0600

    second go around

--- server/server.js
+++ server/server.js
@@ -4,34 +4,9 @@ const session = require("express-session");
 
 const app = express();
 
-const users = [];
-
-//MiddleWare
 app.use(bodyParser.json());
 
-app.use(
-  session({
-    secret: "scoobySnacks",
-    saveUninitialized: true,
-    resave: false
-  })
-);
-
-app.post("/api/login", (req, res) => {
-  const { role } = req.body;
-  req.session.user = {
-    id: req.session.id,
-    role: role
-  };
-  console.log(req.session);
-  //Suhweet!!!
-  res.sendStatus(200);
-});
-
-app.get("/loggedIn/admin", (req, res, next) => {
-  console.log(req.session);
-  res.sendStatus(200);
-});
+//MiddleWare
 
 const port = 3010;
 app.listen(port, () => console.log(`Magic Happens on Port:${port}`));
